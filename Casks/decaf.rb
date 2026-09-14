95c2826a5b0ecaace314dd1dfdd93b884e8a7581
AlanY1an <an1154811577@gmail.com>
2026-09-13 22:37:47 -0500

    Update Decaf to 0.3.4 account-switch fix

--- Casks/decaf.rb
+++ Casks/decaf.rb
@@ -1,6 +1,6 @@
 cask "decaf" do
-  version "0.3.3"
-  sha256 "acbe1c94e4b0685a5c9f572c4fa759469fc958f4a006a74de23f6ab883901daf"
+  version "0.3.4"
+  sha256 "b8bc2c1bb76555b3677bd525511acf076503a37ce4743ffdca726b5f13887343"
 
   url "https://github.com/AlanY1an/decaf/releases/download/v#{version}/Decaf-#{version}.dmg",
       verified: "github.com/AlanY1an/decaf/"
